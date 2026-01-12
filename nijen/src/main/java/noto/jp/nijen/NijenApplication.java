e377c07525d50920f1bec59f635efb3f468ef6a5
halit-windows <halitislam.icli@saglik.gov.tr>
2026-01-12 09:32:32 +0300

    feat: initialize Spring Boot project with Gradle setup, application configuration, and basic test structure

---
+++ nijen/src/main/java/noto/jp/nijen/NijenApplication.java
@@ -0,0 +1,13 @@
+package noto.jp.nijen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NijenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NijenApplication.class, args);
+	}
+
+}
---
+++ nijen/src/test/java/noto/jp/nijen/NijenApplicationTests.java
@@ -0,0 +1,13 @@
+package noto.jp.nijen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NijenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
